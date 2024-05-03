c3edb01d606ad53abe61b5d1390c9e954877b283
kabilesh68 <168735849+kabilesh68@users.noreply.github.com>
2024-05-03 11:32:18 +0530

    Update 71772312141-kabilesh(2)qwestion.py

--- 71772312141-kabilesh(2)qwestion.py
+++ 71772312141-kabilesh(2)qwestion.py
@@ -1,4 +1,5 @@
 import operator
 lst=[('key',101.25),('lock',320.85),('hammer',100.55),('spanner',67.77),('tong',93.05)]
 print(sorted(lst,reverse=True,key=operator.itemgetter(1)))
-      
+output:
+[('lock',320.85),('key',101.25),('hammer',100.55),('tong',93.05),('spanner',67.77)]
